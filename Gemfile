3642d1ef85bc9d0bedeee9e845e73745f639b199
flyingPie23 <drissfreah325@gmail.com>
2024-12-22 00:42:16 +0100

    saved

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem "dotenv-rails"
 
 gem "cloudinary"
 
+gem 'rack-cors', :require => 'rack/cors'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,15 @@ module EcpoBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000' # Your React app's URL
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
+      end
+    end
   end
 end
